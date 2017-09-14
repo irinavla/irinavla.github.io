c6caee9c7a70acc018ec3767c2d88a6d5823f17a
Irina V <irinav0402@gmail.com>
2017-09-14 15:15:12 +0300

    new

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  </head>
+  <body>
+
+    
+<div class="container">
+  <div class="col-md-8 col-md-offset-2">
+  <br />
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+    <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
+    <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
+  </ul>
+
+  <!-- Tab panes -->
+ <div class="tab-content">
+      <div role="tabpanel" class="tab-pane fade in active" id="home"><br />Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores a vero sint eius soluta ab vel perspiciatis iste, totam architecto accusantium numquam officiis, distinctio, amet officia deleniti provident voluptas omnis.</div>
+      <div role="tabpanel" class="tab-pane fade" id="profile"><br />Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque vero debitis at soluta laboriosam minus consequuntur dignissimos eum cupiditate, animi tempora quisquam expedita aut culpa in. Ipsum, quo, quis. Officia.</div>
+      <div role="tabpanel" class="tab-pane fade" id="messages"><br />Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, qui obcaecati voluptates quisquam quia, aspernatur molestiae repellat fugiat error quidem eveniet excepturi suscipit accusamus perferendis sapiente. Assumenda consectetur corporis praesentium.</div>
+      <div role="tabpanel" class="tab-pane fade" id="settings"><br />Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, molestiae officiis, explicabo sint velit quidem earum atque fuga ex quasi provident cumque modi, sapiente dolore. Praesentium illum alias vitae nostrum.</div>
+</div>
+    
+  </div>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+  </body>
+</html>
